36220d056c0c2c90c8601b1b5d61321b6e8868b8
Jack <fabio.costa@wedigi.com.br>
2024-09-18 17:19:21 -0300

    fix: gtm

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -31,7 +31,7 @@ export default defineApp(async (_req, ctx) => {
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://metrics2.trasty.io/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        'https://metrics.trasty.io/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-NP7Q86QP');
         `
         }}>
@@ -134,7 +134,7 @@ export default defineApp(async (_req, ctx) => {
 
       {/* Rest of Preact tree */}
       <ctx.Component />
-      <noscript><iframe src="https://metrics2.trasty.io/ns.html?id=GTM-NP7Q86QP" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <noscript><iframe src="https://metrics.trasty.io/ns.html?id=GTM-NP7Q86QP" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </>
   );
 });
